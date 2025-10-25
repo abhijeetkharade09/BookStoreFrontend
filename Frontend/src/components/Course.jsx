1b3e7594934612f089d6ce9a97be3d9330eff5cc
abhijeetkharade09 <abhijeetkharade2993@gmail.com>
2025-10-25 11:44:22 +0530

    Update Course.jsx

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -11,7 +11,7 @@ const Course = () => {
     useEffect(()=>{
       const getBook = async () => {
         try{
-          const res = await axios.get("http://localhost:4001/book");
+          const res = await axios.get("https://mern-bookstore-backend-8nbj.onrender.com/book");
           console.log(res.data);
           setBook(res.data);
         }catch(error){
